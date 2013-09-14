0971e25a84de136b5ecb81b0ec129da3cfb83fe2
Matt Senior <matt@senior.io>
2013-09-14 14:34:17 +0100

    Add MatchesFormatter to sort results alphabetically or by length

---
+++ test/matches-formatter.js
@@ -0,0 +1,34 @@
+var expect = require('chai').expect;
+
+describe('MatchesFormatter', function() {
+
+    var formatter = require('../lib/matches-formatter');
+    var matches   = ['cat', 'dog', 'a', 'melon'];
+
+    describe('#alphabetical()', function() {
+
+        it('should return results in alphabetical order', function() {
+            expect(formatter.alphabetical(matches)).to.deep.equal([
+                'a', 'cat', 'dog', 'melon'
+            ]);
+        });
+    });
+
+    describe('#longestFirst()', function() {
+
+        it('should return results in size order (longest first) then alphabetically', function() {
+            expect(formatter.longestFirst(matches)).to.deep.equal([
+                'melon', 'cat', 'dog', 'a'
+]);
+        });
+    });
+
+    describe('#shortestFirst()', function() {
+
+        it('should return results in size order (shortest first) then alphabetically', function() {
+            expect(formatter.shortestFirst(matches)).to.deep.equal([
+                'a', 'cat', 'dog', 'melon'
+            ]);
+        });
+    });
+});
